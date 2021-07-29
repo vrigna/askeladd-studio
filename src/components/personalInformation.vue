3be01f6171c51ceda9778a7ed3c9d512aface92a
josh-carrizo <joshuacarrizo@gmail.com>
2021-07-29 10:29:50 -0400

    Agregue fotos y algo de estilo al Cultura

--- src/components/personalInformation.vue
+++ src/components/personalInformation.vue
@@ -1,12 +1,18 @@
 <template>
-    <div class="row container">
-        <img src="" class="img-thumbnail img col-3" alt="..."/>
-        <div class="col-2"/>
-        <div class="col-7">
-            <h4>{{ title }}</h4>
-            <p>{{ contents }}</p>
-            <p>{{ name }}</p>
-            <p>{{ job }}</p>
+    <div class="container">
+        <div class="row">
+            <img 
+                :src="require('@/assets/' + imagen + '')" 
+                :alt="name" 
+                class="img col-3 rounded-circle mb-4" 
+            >
+            <div class="col-1"/>
+            <div class="col-7">
+                <h4 class="font-weight-bold text-primary display-5 " style="font-family: Poppins">{{ title }}</h4>
+                <p>{{ contents }}</p>
+                <span class="d-block font-weight-bold" style="font-family: Poppins">{{ name }}</span>
+                <p class="text-primary d-block font-weight-bold"> {{ job }}</p>
+            </div>
         </div>
     </div>
 
@@ -16,6 +22,7 @@
 export default {
     name:"personalInformation",
     props: {
+        imagen: String,
         title: String,
         contents: String,
         name: String,
@@ -25,6 +32,13 @@ export default {
 }
 </script>
 
-<style>
+<style scooped>
+.img{
+    /*Sombra morada --> filter: drop-shadow(16px 16px 20px rgb(64, 44, 139));
+    background: rgb(64, 44, 139);
+    filter: grayscale(100%) brightness(40%) sepia(100%) hue-rotate(-50deg) saturate(600%) contrast(0.8);*/
+    filter: drop-shadow(16px 16px 20px rgb(18, 14, 32));
+    max-width:15rem;
+}
+</style>>
 
-</style>
--- src/views/Culture.vue
+++ src/views/Culture.vue
@@ -1,14 +1,61 @@
 <template>
   <div class="d-flex flex-column justify-content-center bg-light">
-      <div class="container"></div>
-      <h2>Nuestra cultura</h2>
-      <h4>Contada por nuestro propio equipo (Terrible Joya)</h4>
-      <personalInformation :imagen="imagen_carlos" :title="title_carlos" :contents="contents_carlos" :name="name_carlos" :job="job_carlos" />
-      <personalInformation :imagen="imagen_jazzmin" :title="title_jazzmin" :contents="contents_jazzmin" :name="name_jazzmin" :job="job_jazzmin"/>
-      <personalInformation :imagen="imagen_josh" :title="title_josh" :contents="contents_josh" :name="name_josh" :job="job_josh"/>
-      <personalInformation :imagen="imagen_juan" :title="title_juan" :contents="contents_juan" :name="name_juan" :job="job_juan"/>
-      <personalInformation :imagen="imagen_german" :title="title_german" :contents="contents_german" :name="name_german" :job="job_german"/>
-      <personalInformation :imagen="imagen_edison" :title="title_edison" :contents="contents_edison" :name="name_edison" :job="job_edison"/>
+      <div class="container">
+        <h2 class="font-weight-bold text-primary display-4 text-center" style="font-family: Poppins">Nuestra cultura</h2>
+        <h4 class="text-center" style="font-family: Poppins">Contada por nuestro propio equipo (Terrible Joya)</h4>
+        <br><br>
+        <personalInformation 
+            :imagen="faceName_carlos"
+            :title="title_carlos"
+            :contents="contents_carlos" 
+            :name="name_carlos" 
+            :job="job_carlos" />
+
+        <br>
+
+        <personalInformation 
+            :imagen="faceName_jazzmin"
+            :title="title_jazzmin" 
+            :contents="contents_jazzmin" 
+            :name="name_jazzmin" 
+            :job="job_jazzmin"/>
+
+        <br>
+
+        <personalInformation 
+            :imagen="faceName_josh"
+            :title="title_josh" 
+            :contents="contents_josh" 
+            :name="name_josh" 
+            :job="job_josh"/>
+
+        <br>    
+
+        <personalInformation 
+            :imagen="faceName_juan"
+            :title="title_juan" 
+            :contents="contents_juan" 
+            :name="name_juan" 
+            :job="job_juan"/>
+
+        <br>
+
+        <personalInformation 
+            :imagen="faceName_german"
+            :title="title_german" 
+            :contents="contents_german" 
+            :name="name_german" 
+            :job="job_german"/>
+
+        <br>
+        
+        <personalInformation 
+            :imagen="faceName_edison"
+            :title="title_edison" 
+            :contents="contents_edison" 
+            :name="name_edison" 
+            :job="job_edison"/>
+      </div>
 
   </div>
 </template>
@@ -27,36 +74,42 @@ export default {
             name_carlos: null,
             job_carlos: null,
             imagen_carlos: null,
+            faceName_carlos: null,
 
             title_jazzmin: null,
             contents_jazzmin: null,
             name_jazzmin: null,
             job_jazzmin: null,
             imagen_jazzmin: null,
+            faceName_jazzmin: null,
 
             title_josh: null,
             contents_josh: null,
             name_josh: null,
             job_josh: null,
             imagen_josh: null,
+            faceName_josh: null,
 
             title_juan: null,
             contents_juan: null,
             name_juan: null,
             job_juan: null,
             imagen_juan: null,
+            faceName_juan: null,
 
             title_german: null,
             contents_german: null,
             name_german: null,
             job_german: null,
             imagen_german: null,
+            faceName_german: null,
 
             title_edison: null,
             contents_edison: null,
             name_edison: null,
             job_edison: null,
-            imagen_edison: null
+            imagen_edison: null,
+            faceName_edison: null
         }
     },
     components: {
@@ -70,37 +123,37 @@ export default {
         this.contents_carlos = datos.personal[0].contents
         this.name_carlos = datos.personal[0].name
         this.job_carlos = datos.personal[0].job
-        this.imagen_carlos = datos.personal[0].imagen
+        this.faceName_carlos = datos.personal[0].imagen
 
-        this.title_jazzim = datos.personal[1].title
-        this.contents_jazzim = datos.personal[1].contents
-        this.name_jazzim = datos.personal[1].name
-        this.job_jazzim = datos.personal[1].job
-        this.imagen_jazzim = datos.personal[1].imagen
+        this.title_jazzmin = datos.personal[1].title
+        this.contents_jazzmin = datos.personal[1].contents
+        this.name_jazzmin = datos.personal[1].name
+        this.job_jazzmin = datos.personal[1].job
+        this.faceName_jazzmin = datos.personal[1].imagen
 
         this.title_josh = datos.personal[2].title
         this.contents_josh = datos.personal[2].contents
         this.name_josh = datos.personal[2].name
         this.job_josh = datos.personal[2].job
-        this.imagen_josh = datos.personal[2].imagen
+        this.faceName_josh = datos.personal[2].imagen2
 
         this.title_juan = datos.personal[3].title
         this.contents_juan = datos.personal[3].contents
         this.name_juan = datos.personal[3].name
         this.job_juan = datos.personal[3].job
-        this.imagen_juan = datos.personal[3].imagen
+        this.faceName_juan = datos.personal[3].imagen
 
         this.title_german = datos.personal[4].title
         this.contents_german = datos.personal[4].contents
         this.name_german = datos.personal[4].name
         this.job_german = datos.personal[4].job
-        this.imagen_german = datos.personal[4].imagen
+        this.faceName_german = datos.personal[4].imagen
 
         this.title_edison = datos.personal[5].title
         this.contents_edison = datos.personal[5].contents
         this.name_edison = datos.personal[5].name
         this.job_edison = datos.personal[5].job
-        this.imagen_edison = datos.personal[5].imagen
+        this.faceName_edison = datos.personal[5].imagen
 
     }
 
