79880b7734ce4a9bce72a18406c9f2231687d743
Jazmin <jazmin.jahazieltru@gmail.com>
2021-08-02 18:11:11 -0400

    changes in footer icons

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,21 +1,20 @@
 <template>
 
 <footer class="text-center text-white bg-dark">
-  <div class="container pt-5 pb-2 text-center " >
-    <a class="li-ic"> 
+  <div class="container pt-5 pb-2 text-center" >
+    <a href="https://www.youtube.com/watch?v=Jdf9PadGu_c"> 
       <font-awesome-icon :icon="[ 'fab', 'youtube' ]" class="fa-4x px-2"/>
     </a>
-    <a class="li-ic">
+    <a href="https://github.com/vrigna/askeladd-studio">
       <font-awesome-icon :icon="[ 'fab', 'github' ]" class="fa-4x px-2"/>
     </a>
-    <a class="li-ic">
+    <a href="#">
       <font-awesome-icon :icon="[ 'fab', 'twitter' ]" class="fa-4x px-2"/>
     </a>
-    <a class="li-ic">
+    <a href="#">
       <font-awesome-icon :icon="[ 'fab', 'vuejs' ]" class="fa-4x px-2"/>
     </a>
   </div>
-  <!--texto askeladd--->
   <div class="text-center font text-light p-1 pb-5  font-size">
     2021
     <a class="text-white " href="https://askeladdStudio.com/">Askeladd, inc.</a>
@@ -36,7 +35,13 @@ export default {
   @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap');
 
   .font{
-  font-family: 'Poppins';
+    font-family: 'Poppins';
+  }
+  a{
+    color: white;
+  }
+  a:hover{
+    color: #D1BCFE;
   }
 
 </style>
