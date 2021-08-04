2b33fbf394720dd03f9e724c944402af647e2518
josh-carrizo <joshuacarrizo@gmail.com>
2021-08-04 19:25:57 -0400

    Actualizando Content View

--- src/views/ContentView.vue
+++ src/views/ContentView.vue
@@ -16,32 +16,33 @@
         </div>
 
       </div>
-          <div class="col-5">
-            <br><br>
-            <h3 class="mb-3 text-primary d-block font-weight-bold" style="font-family: Poppins"> Desde Askeladd hacia la comunidad</h3>
-            <p>En Askeladd nos preocupamos por documentar procesos, crear apuntes guía, y crear contenido en forma de post o video, que sirva tanto a nuesto equipo interno como a cualquier persona que lo necesite.</p>
-            <p>Es por eso que habilitamos un <b>Notion</b> abierto, en el que podrás encontrar una sección de contenido con apuntes y  paso a pasos. Una biblioteca con libros del mejor nivel y sobre muchos géneros. Y por último, encontraras recursos utiles para la etapa de desarrollo y diseño. Esperamos que lo disfrutes! </p>
-            <div class="row">
-                <div class="col-6">
-                  <hr>
-                  <h4 class="mb-3 text-primary d-block font-weight-bold" style="font-family: Poppins"> Apuntes </h4>
-                  <h3 class="mb-3 d-block font-weight-bold" style="font-family: Poppins"> 24</h3>
-                  <br>
-                  <hr>
-                  <h4 class="mb-3 text-primary d-block font-weight-bold" style="font-family: Poppins"> Post </h4>
-                  <h3 class="mb-3 d-block font-weight-bold" style="font-family: Poppins"> 36</h3>
-                </div>
-                <div class="col-6">
-                  <hr>
-                  <h4 class="mb-3 text-primary d-block font-weight-bold" style="font-family: Poppins"> Trabajadores </h4>
-                  <h3 class="mb-3 d-block font-weight-bold" style="font-family: Poppins"> 6 </h3>
-                  <br>
-                  <hr>
-                  <h4 class="mb-3 text-primary d-block font-weight-bold" style="font-family: Poppins"> Proyectos </h4>
-                  <h3 class="mb-3 d-block font-weight-bold" style="font-family: Poppins"> 2</h3>
-                </div>
+      <div class="col"></div>
+      <div class="col-5">
+        <br><br>
+        <h3 class="mb-3 text-primary d-block font-weight-bold" style="font-family: Poppins"> Desde Askeladd hacia la comunidad</h3>
+        <p>En Askeladd nos preocupamos por documentar procesos, crear apuntes guía, y crear contenido en forma de post o video, que sirva tanto a nuesto equipo interno como a cualquier persona que lo necesite.</p>
+        <p>Es por eso que habilitamos un <b>Notion</b> abierto, en el que podrás encontrar una sección de contenido con apuntes y  paso a pasos. Una biblioteca con libros del mejor nivel y sobre muchos géneros. Y por último, encontraras recursos utiles para la etapa de desarrollo y diseño. Esperamos que lo disfrutes! </p>
+        <div class="row">
+            <div class="col-6">
+              <hr>
+              <h4 class="mb-3 text-primary d-block font-weight-bold" style="font-family: Poppins"> Apuntes </h4>
+              <h3 class="mb-3 d-block font-weight-bold" style="font-family: Poppins"> 24</h3>
+              <br>
+              <hr>
+              <h4 class="mb-3 text-primary d-block font-weight-bold" style="font-family: Poppins"> Post </h4>
+              <h3 class="mb-3 d-block font-weight-bold" style="font-family: Poppins"> 36</h3>
             </div>
-          </div>
+            <div class="col-6">
+              <hr>
+              <h4 class="mb-3 text-primary d-block font-weight-bold" style="font-family: Poppins"> Trabajadores </h4>
+              <h3 class="mb-3 d-block font-weight-bold" style="font-family: Poppins"> 6 </h3>
+              <br>
+              <hr>
+              <h4 class="mb-3 text-primary d-block font-weight-bold" style="font-family: Poppins"> Proyectos </h4>
+              <h3 class="mb-3 d-block font-weight-bold" style="font-family: Poppins"> 2</h3>
+            </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -58,7 +59,7 @@ hr {
   background-color: black; 
 }
 .img-content {
-  height: 18rem;
+  height: 21rem;
   filter: grayscale(100%);
   
 }
