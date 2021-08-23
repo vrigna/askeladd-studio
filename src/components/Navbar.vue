d033b9554865b162cbd8ff9e4964ff638bed71aa
Carlos Brignardello <carlos.alb.brig@gmail.com>
2021-08-23 19:41:48 -0400

    se modifico el contenido

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,7 +4,9 @@
     <nav class="fixed-top navbar navbar-expand-lg navbar-light bg-light shadow p-3 bg-body rounded">
         <div class="container fluid justify-content-evenly">
             <img alt="Askeladd logo" src="../assets/logo.png" height="40px">
-            
+            <button class="navbar-toggler" type="button">
+                <span class="navbar-toggler-icon"></span>
+            </button>
             <div class="d-none d-lg-block " id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item font-weight-bold">
@@ -27,9 +29,7 @@
                     </li>
                 </ul>
             </div>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+
         </div>
     </nav>
 
--- src/views/ContentPosts.vue
+++ src/views/ContentPosts.vue
@@ -2,40 +2,69 @@
   <div class="min-vh-100 bg-light" style="padding-top: 100px;">
     <div class="px-5">
       <div class="row">
-        <div class="col-3 bg-primary p-0 h-100 text-center rounded">
-          <div class="bg-dark rounded-top" style="height: 20px;">
-          </div>
-          <nav>
-            <ul class="list-unstyled">
-              <li class="text-uppercase p-3 font-weight-bold">
+        <div class="col-3 p-0 h-100 text-center rounded">
+          <nav class="bg-dark">
+            <ul class="list-unstyled m-0">
+              <li class="text-uppercase lista p-3 font-weight-bold">
                 <a href="">publicaciones</a>
               </li>
-              <li class="text-uppercase p-3 font-weight-bold">
+              <li class="text-uppercase lista p-3 font-weight-bold">
                 <a href="">apuntes</a>
               </li>
-              <li class="text-uppercase p-3 font-weight-bold">
+              <li class="text-uppercase lista p-3 font-weight-bold">
                 <a href="">buenas practicas</a>
               </li>
-              <li class="text-uppercase p-3 font-weight-bold">
+              <li class="text-uppercase lista p-3 font-weight-bold">
                 <a href="">notas rapidas</a>
               </li>
             </ul>
           </nav>
+
+          <div class="mt-5 pt-4 bg-dark">
+            <h2 class="text-light pb-5 text-capitalize font-weight-bold" style="font-family: Poppins" >categorias</h2>
+          <div class="row">
+            <div class="col-6 mb-4">
+              <div class="ml-4 p-2 font-weight-bold" style="background-color: #28ffbf; border-radius: 100px;">
+                <span>Front End</span>
+              </div>
+            </div>
+            <div class="col-6 mb-4">
+              <div class="mr-4 p-2 font-weight-bold" style="background-color: #BCFFB9; border-radius: 100px;">
+                <span>Back End</span>
+              </div>
+            </div>
+            <div class="col-6 mb-4">
+              <div class="ml-4 p-2 font-weight-bold" style="background-color: #F7E6AD; border-radius: 100px;">
+                <span>IA</span>
+              </div>
+            </div>
+            <div class="col-6 mb-4">
+              <div class="mr-4 p-2 font-weight-bold" style="background-color: #F8A1D1; border-radius: 100px;">
+                <span>CRIPTO</span>
+              </div>
+            </div>
+            <div class="col-6 mb-4">
+              <div class="ml-4 p-2 font-weight-bold" style="background-color: #822659; border-radius: 100px;">
+                <span>Crecimiento</span>
+              </div>
+            </div>
+            <div class="col-6 mb-4">
+              <div class="mr-4 p-2 font-weight-bold" style="background-color: #B34180; border-radius: 100px;">
+                <span>Podcast</span>
+              </div>
+            </div>
+           </div>
+          </div>
         </div>
         <div class="col-9">
-          <div class="">
-            <div class="bg-primary rounded-top" style="height: 20px;"></div>
-            <div class="p-4 border rounded-bottom" style="background-color: white;">
-              <h2 class="m-0 font-weight-bold text-primary" style="font-family: Poppins;">Estas son nuestras publicaciones</h2>
-            </div>
-          </div>
+
 
           <!-- <div class="mt-4 p-5 filters bg-primary w-100">
             <h2>Filtrar</h2>
 
           </div> -->
 
-          <div class="row m-0 mt-3">
+          <div class="row m-0">
             <ContentCard 
               author="Carlos Brignardello" 
               title="How to read Member-only Medium Stories for free without being a member?" 
@@ -73,5 +102,7 @@ export default {
 </script>
 
 <style>
-
+.lista:hover{
+  background-color: #734EDC;
+}
 </style>
